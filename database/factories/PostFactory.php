120d6ed3dbd8e8150f474fa69988b7a34bc80656
ahmad.psmd <ahmaadzaid7@gmail.com>
2024-02-21 14:56:01 +0300

    finished lession 5

---
+++ database/factories/PostFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Post>
+ */
+class PostFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        // $categories = Category::pluck('id');
+
+        return [
+            'title'       => $this->faker->word(),
+            'content'     => $this->faker->paragraphs(asText: true),
+            // 'category_id' => $categories->random(),
+        ];
+    }
+}
