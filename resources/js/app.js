6043651e22512c5fbb5bbf1255122659f25bfb1d
ahmad.psmd <ahmaadzaid7@gmail.com>
2024-02-22 13:56:33 +0300

    finish lesson 12

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,18 +1,7 @@
 import "./bootstrap";
 
 import { createApp } from "vue";
-import { createRouter, createWebHistory } from "vue-router";
 import App from "./layouts/App.vue";
+import router from "./routes/index";
 
-import PostsIndex from "./components/Posts/Index.vue";
-import PostsCreate from "./components/Posts/Create.vue";
-const routes = [
-    { path: "/", component: PostsIndex },
-    { path: "/posts/create", component: PostsCreate },
-];
-
-const router = createRouter({
-    history: createWebHistory(),
-    routes,
-});
 createApp(App).use(router).mount("#app");
