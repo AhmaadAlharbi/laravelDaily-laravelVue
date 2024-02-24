9d8b977b84e3c35f757af91a9c00d0e87a5e7289
AhmaadAlharbi <ahmaadzaid7@gmail.com>
2024-02-24 18:43:58 +0300

    finish lesson 22

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,8 +1,7 @@
 import "./bootstrap";
 
 import { createApp } from "vue";
-import App from "./layouts/App.vue";
 import router from "./routes/index";
 import VueSweetalert2 from "vue-sweetalert2";
 
-createApp(App).use(router).use(VueSweetalert2).mount("#app");
+createApp({}).use(router).use(VueSweetalert2).mount("#app");
---
+++ resources/js/components/Auth/Login.vue
@@ -0,0 +1,1 @@
+<template>Login form.</template>
